6b617c68b168fc8ac4888705d5469f3ac14af7f7
Cuong Pham <cuong.duc.pham@gmail.com>
2014-06-01 11:04:15 +1000

    change ww crawler to crawl top level categories only

--- grocerices_crawler/spiders/ww.py
+++ grocerices_crawler/spiders/ww.py
@@ -1,26 +1,43 @@
-from scrapy.selector import Selector
+from scrapy.contrib.spiders import CrawlSpider
+from scrapy.contrib.spiders import Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
-from scrapy.contrib.spiders import CrawlSpider, Rule
-from demo.items import DemoItem
-from scrapy.selector import XmlXPathSelector
+from scrapy.selector import Selector
 
-from demo.items import DemoItem
+from groceries_crawler.items import GroceriesCrawlerItem
+import re
 
-class WwSpider(CrawlSpider):
+
+'''
+Only process top level Categories, lower level categories contains products from top level
+'''
+def process_link(link):
+    pattern = "/Shop/Browse/\w+$"
+    if re.match(pattern,link):
+        return link
+    else:
+        return None
+
+class WoolworthSpider(CrawlSpider):
     name = 'ww'
     allowed_domains = ['woolworthsonline.com.au']
-    start_urls = ['http://www2.woolworthsonline.com.au/Shop/Browse/bakery']
-
-    rules = (
-        Rule(SgmlLinkExtractor(restrict_xpaths=('//a[@class="_jumpTop"]')),callback='parse_item', follow=True),
-    )
+    start_urls = [
+        "http://www2.woolworthsonline.com.au/"
+    ]
+    rules = [
+                Rule(SgmlLinkExtractor(restrict_xpaths=('//a[@class="navigation-link"]'),
+                                       process_value=process_link)
+                ),
+                Rule(SgmlLinkExtractor(restrict_xpaths=('//a[@class="_jumpTop"]')),
+                    follow=True, callback="parse_item"
+                )
+    ]
 
     def parse_item(self, response):
         sel = Selector(response=response)
         products = sel.xpath("""//div[contains(@class,"product-stamp-middle")]""")
         items = []
         for product in products:
-            item = DemoItem()
+            item = GroceriesCrawlerItem()
             item['name'] = product.xpath(""".//span[contains(@class,"description")]/text()""").extract()
             item['price'] = product.xpath(""".//span[contains(@class,"price")]/text()""").extract()
             items.append(item)
